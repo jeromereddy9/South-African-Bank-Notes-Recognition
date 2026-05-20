547eaf2d9bf14e8ad7a8b76fb778f65e7f130ddb
JPR <72471169+jeromereddy9@users.noreply.github.com>
2026-05-20 19:56:44 +0200

    Project update

---
+++ South-African-Bank-Notes-Recognition/Data/segmentation.py
@@ -0,0 +1,19 @@
+
+
+def detect_edges_canny(image, low_threshold, high_threshold):
+    pass
+
+def deskew(image, contour):
+    pass
+
+def segment_note(image, method='canny'):
+    pass
+
+def crop_to_contour(image, contour):
+    pass
+
+def find_largest_contour(edge_map):
+    pass
+
+def detect_edges_sobel(image):
+    pass
